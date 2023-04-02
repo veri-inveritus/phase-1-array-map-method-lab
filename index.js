c8b333c43f96948d7071e528494724ddc2bc7567
David <yidavidh@outlook.com>
2023-04-02 10:12:51 -0700

    Lab Completed

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = () => 
+  tutorials.map(tutorial =>
+    tutorial
+      .split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ')
+  );
+
